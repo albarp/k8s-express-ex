18f53b210b6ae1e2a3f4dfd548df9c3beda4bc76
Alberto Arpini <alberto.arpini@freedamedia.com>
2022-12-09 18:06:44 +0100

    added env variables

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log(`AppName: ${process.env.APP_NAME}`)
+  console.log(`Username: ${process.env.USERNAME}`)
+  console.log(`Password: ${process.env.PASSWORD}`)
   res.send('Hello World4!')
 })
 
